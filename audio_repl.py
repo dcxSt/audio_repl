c77817df089414e544ab124905ba95d6b19cc9be
Stephen Fay <dcxstephen@gmail.com>
2024-03-08 16:36:26 -0800

    send message to kernel on localhost while recording audio

--- audio_repl.py
+++ audio_repl.py
@@ -2,6 +2,18 @@ import sounddevice as sd
 from scipy.io.wavfile import write
 import numpy as np
 from datetime import datetime
+from time import time
+import json
+import struct
+import socket
+
+# kernel parameters
+HOST = '127.0.0.1' # data.acquisition.host
+PORT = 6767
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((HOST, PORT))
+
+id = 1 # event id?
 
 # Parameters
 FS = 44100  # Sample rate
@@ -15,9 +27,10 @@ def callback(indata, frames, time, status):
     recorded_frames.append(indata.copy())
 
 def start_audio_recording():
+    global id
     stream = sd.InputStream(samplerate=FS, channels=channels, callback=callback)
     stream.start()
-    filename = f'out_{int(datetime.now().timestamp() * 1000)}_ctime_start_ms.wav'
+    filename = f'out_start_mus_{int(datetime.now().timestamp() * 1e6)}_id_{str(id).zfill(3)}.wav'
     print("Recording audio...")
     return stream, filename 
 
@@ -27,6 +40,40 @@ def stop_audio_recording(stream, filename):
     audio_data = np.concatenate(recorded_frames, axis=0)
     write(filename, FS, audio_data)
     print(f"Audio recording saved as {filename}")
+
+def start():
+    global id
+    stream, filename = start_audio_recording()
+    data_to_send = {
+      "id": id,
+      "timestamp": int(time() * 1e6),
+      "event": "start_experiment",
+      "value": "start_audio"
+    }
+    print(f"data = {data_to_send}") # debug
+    event = json.dumps(data_to_send).encode("utf-8")
+    msg = struct.pack("!I", len(event)) + event
+    sock.sendall(msg)
+    id += 1
+    print("start message sent to kernel")
+    return stream, filename
+
+def stop(stream, filename):
+    global id
+    data_to_send = {
+      "id": id,
+      "timestamp": int(time() * 1e6),
+      "event": "end_experiment",
+      "value": "stop_audio"
+    }
+    print(f"data = {data_to_send}") # debug
+    event = json.dumps(data_to_send).encode("utf-8")
+    msg = struct.pack("!I", len(event)) + event
+    sock.sendall(msg)
+    id += 1
+    print("stop message sent to kernel, ending audio...")
+    stop_audio_recording(stream, filename)
+    print("done")
 
 ## Create the stream object
 #stream = sd.InputStream(samplerate=FS, channels=channels, callback=callback)
@@ -46,18 +93,18 @@ def repl():
             if stream is not None:
                 print("Recording already in progress.")
             else:
-                stream, filename = start_audio_recording()
+                stream, filename = start()
         elif command == "stop":
             if stream is None:
                 print("No recording in progress.")
             else:
-                stop_audio_recording(stream, filename)
+                stop(stream, filename)
                 stream = None
                 print("Enter 'start' to start anew, 'exit' to quit.")
         elif command == "exit":
             if stream is not None:
                 print("Stopping recording before exit...")
-                stop_audio_recording(stream, filename)
+                stop(stream, filename)
             break
 
 if __name__ == "__main__":
